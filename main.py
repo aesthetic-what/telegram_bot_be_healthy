9975fd4023c2ccae194f5e1661a72ddd41272819
aesthetic-what <ladnoladno477@gmail.com>
2024-09-16 00:40:15 +0500

    main.py

--- main.py
+++ main.py
@@ -35,6 +35,6 @@ def webhook():
 if __name__ == '__main__':
     try:
         asyncio.run(main())
-        app.run(host='0.0.0.0', port=90)
+        app.run(host='0.0.0.0', port=80)
     except KeyboardInterrupt:
         print("bot deactivated")
