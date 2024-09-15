943ed0c8ee2bc7f1a71851708fbaca7d39d557f3
aesthetic-what <ladnoladno477@gmail.com>
2024-09-16 00:09:15 +0500

    main.py

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
-import asyncio
-from flask import Flask
+from flask import Flask, request
 from aiogram import Bot, Dispatcher
+import requests
+from app.handlers import router
 
-from handlers import router
-
+TOKEN = Bot(token='7530313667:AAFQz08Gnrv-Mqty2IA1cT1COzXqgYo84I8')
 
 async def main():
-    bot = Bot(token='7317856959:AAEm6qiBoD0MwBHDMV61LJf3bI0-JwvpcaY')
+    bot = Bot(token='7530313667:AAFQz08Gnrv-Mqty2IA1cT1COzXqgYo84I8')
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
 
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+def webhook():
+    data = request.get_json()
+
+    if 'message' in data:
+        message_text = data['message']['text']
+        chat_id = data['message']['chat']['id']
+        # Отправка ответа в Telegram
+        response = requests.post(
+            f'https://api.telegram.org/bot{TOKEN}/sendMessage',
+            json={'chat_id': chat_id, 'text': f'Вы написали: {message_text}'}
+        )
+        return 'OK', 200
+
+    return 'OK', 200
 
 
 if __name__ == '__main__':
     try:
-        asyncio.run(main())
-        app.run(debug=True, port=5000)
+        app.run(host='0.0.0.0', port=80)
     except KeyboardInterrupt:
         print("bot deactivated")
