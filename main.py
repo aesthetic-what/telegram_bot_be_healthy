2106ca89eb320489d596c7ff95afc5537a56ec11
aesthetic-what <ladnoladno477@gmail.com>
2024-09-15 22:49:35 +0500

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import asyncio
+from flask import Flask
 from aiogram import Bot, Dispatcher
 
-from handlers import router
+from app.handlers import router
 
 
 async def main():
@@ -10,9 +11,17 @@ async def main():
     dp.include_router(router)
     await dp.start_polling(bot)
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
 
 if __name__ == '__main__':
     try:
         asyncio.run(main())
+        app.run(debug=True, port=5000)
     except KeyboardInterrupt:
         print("bot deactivated")
