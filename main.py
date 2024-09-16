1b1e0509e7fcb9f14194f03aac275ba67f9e2df6
aesthetic-what <ladnoladno477@gmail.com>
2024-09-16 11:33:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,20 @@
-import uvicorn
 import asyncio
 import requests
 from handlers import router
-from aiogram.utils import executor
-from aiogram.types import messages
-from aiogram import Bot, Dispatcher, types
-from fastapi import FastAPI, Request
+from aiogram import Bot, Dispatcher
 
-TOKEN = Bot(token='7530313667:AAG-kqhEUNv0qi3N5s8ARA8Gn1XpxyVpOwU')
 
 async def main():
     bot = Bot(token='7530313667:AAG-kqhEUNv0qi3N5s8ARA8Gn1XpxyVpOwU')
     dp = Dispatcher()
     dp.include_router(router)
-    uvicorn.run(app, host="0.0.0.0", port=8000)
     await dp.start_polling(bot) 
 
-app = FastAPI()
-
-# Функция для обработки webhook запросов
-@app.post("/webhook")
-async def webhook(request: Request):
-    data = await request.json()
-    if "message" in data:
-        message_text = data["message"]["text"]
-        chat_id = data["message"]["chat"]["id"]
-        await bot.send_message(chat_id=chat_id, text=f"Вы написали: {message_text}")
-    return "OK"
 
 
 if __name__ == '__main__':
-    executor.start_webhook(dispatcher=dp, webhook_path="/",
-                          on_startup=bot.set_webhook, 
-                          on_shutdown=bot.delete_webhook,
-                          skip_updates=True, 
-                          host="0.0.0.0", port=8000, url_path='/webhook')
+    try:
+        # Запуск основного цикла бота
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        print("Бот остановлен")
