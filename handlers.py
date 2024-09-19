e8d288a3201c867d439ba674c8eff06c5490b4d7
aesthetic-what <ladnoladno477@gmail.com>
2024-09-19 13:43:40 +0500

    Add files via upload

--- handlers.py
+++ handlers.py
@@ -5,13 +5,16 @@ from aiogram.filters import CommandStart
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 
-import keyboard as kb
+import app.keyboard as kb
 
 router = Router()
+
+
 # user_choices = {}  # Словарь для хранения выборов пользователей
 
 
 class Health(StatesGroup):
+    doctor = State()
     name = State()
     age = State()
     blood_pressure1 = State()
@@ -22,16 +25,15 @@ class Health(StatesGroup):
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer(f'привет, я бот-помощник, группы "Челябинск, не болей!'
-                         'можете ознакомится с видео-инструкцией как нужно замерять давление: '
+    await message.answer('Можете ознакомится с видео-инструкцией как нужно измерять давление: '
                          'https://vk.ru/video/@0chereshnya0?section=upload&z=video432562512_456239749',
                          reply_markup=kb.main)
 
 
-@router.message(F.text == 'Узнать свое состояние')
+@router.message(F.text == 'Оценить уровень артериального давления')
 async def health(message: Message, state: FSMContext):
     await state.set_state(Health.name)
-    await message.answer("Введите свое имя")
+    await message.answer("Укажите свое имя")
 
 
 @router.message(F.text == "О нас")
@@ -59,7 +61,7 @@ async def health_name(message: Message, state: FSMContext):
 
 
 @router.message(Health.age)
-async def health_name(message: Message, state: FSMContext):
+async def health_age(message: Message, state: FSMContext):
     print(message.text)
     try:
         age = int(message.text)
@@ -76,11 +78,11 @@ async def health_name(message: Message, state: FSMContext):
 
     await state.update_data(age=message.text)
     await state.set_state(Health.blood_pressure1)
-    await message.answer('Введите свое систолическое артериальное давление')
+    await message.answer('Ваше систолическое (верхнее) давление')
 
 
 @router.message(Health.blood_pressure1)
-async def health_name(message: Message, state: FSMContext):
+async def health_pressure1(message: Message, state: FSMContext):
     print(message.text)
     try:
         print(type(message.text))
@@ -98,11 +100,11 @@ async def health_name(message: Message, state: FSMContext):
 
     await state.update_data(blood_pressure1=message.text)
     await state.set_state(Health.blood_pressure2)
-    await message.answer('Введите свое диастолическое артериальное давление')
+    await message.answer('Ваше диастолическое (нижнее) давление')
 
 
 @router.message(Health.blood_pressure2)
-async def health_pressure2(message: Message, state: FSMContext):
+async def health_answer(message: Message, state: FSMContext):
     print(message.text)
     try:
         if int(message.text) < 40:
@@ -120,15 +122,17 @@ async def health_pressure2(message: Message, state: FSMContext):
 
     await state.update_data(blood_pressure2=message.text)
     data = await state.get_data()
-    await message.answer("выберите болезни, если таковые есть:", reply_markup=kb.illnesses_keyboard)
 
     sys_pressure = int(data["blood_pressure1"])
     dia_pressure = int(data["blood_pressure2"])
 
     bp_category = categorize_blood_pressure(sys_pressure, dia_pressure)
 
-    # Если категория "АГ 3-й степени", сразу выводим результат и завершаем процесс
-    if bp_category == "АГ 3-й степени":
+    if bp_category != "АГ 3-й степени":
+        await message.answer("Ставил ли Вам врач какой-то из следующих диагнозов?:", reply_markup=kb.illnesses_keyboard)
+
+    else:
+        # Если категория "АГ 3-й степени", сразу выводим результат и завершаем процесс
         await message.answer(
             "У вас обнаружена АГ 3-й степени. "
             "Сопровождается ли повышение давления головной болью, головокружением, потемнением в глазах, "
@@ -148,15 +152,33 @@ async def handle_ag_3_degree(message: Message, data: dict) -> str:
     dia_pressure = data["blood_pressure2"]
     # Формируем сообщение в зависимости от наличия симптомов
     if symptoms:
-        conclusion = "У вас АГ 3-й степени с опасными симптомами. Необходим вызов скорой медицинской помощи (112, 103)."
+        warning = ("Ваш уровень артериального давления (АД) намного выше целевых значений. Рекомендуем Вам "
+                   "регулярно изменять давление утром и вечером, а также вести дневник АД и частоты сердечных "
+                   "сокращений.")
+
+        conclusion = ("По описанным Вами жалобам наблюдаются симптомы гипертонического криза.\nЭто тяжелое "
+                      "состояние, требующее вызова скорой медицинской помощи (тел. 103, 112).\n"
+                      "Рекомендуем Вам регулярно изменять давление утром и вечером, "
+                      "а также вести дневник АД и частоты сердечных сокращений.\n"
+                      "\n"
+                      "Хотели бы вы записаться на прием к врачу?")
+
     else:
-        conclusion = ("У вас АГ 3-й степени без опасных симптомов. "
-                      "Требуется немедленная коррекция лечения, обратитесь к врачу.")
+        warning = ("Ваш уровень артериального давления (АД) намного выше целевых значений. Рекомендуем Вам "
+                   "регулярно изменять давление утром и вечером, а также вести дневник АД и частоты сердечных "
+                   "сокращений.")
+
+        conclusion = ("Требуется назначение или коррекция лечения. "
+                      "Обратитесь за консультацией к лечащему врачу в ближайшее время"
+                      "\n"
+                      "Хотели бы вы записаться на прием к врачу?")
 
     # Возвращаем итоговое заключение
     return (f"Имя: {name}\nВозраст: {age}\n"
             f"Ваше давление: {sys_pressure}/{dia_pressure}\n"
-            f"Заключение:\n{conclusion}")
+            f"Заключение:\n{warning}"
+            '\n'
+            f"\n{conclusion}\n")
 
 
 @router.message(Health.symptoms)
@@ -173,7 +195,7 @@ async def health_symptoms(message: Message, state: FSMContext):
 
     # Обрабатываем данные и выводим итоговый результат с учетом симптомов
     health_status = await handle_ag_3_degree(message, data)
-    await message.answer(health_status, reply_markup=kb.main)
+    await message.answer(health_status, reply_markup=kb.SMD)
 
     # Очищаем состояние после вывода данных
     await state.clear()
@@ -198,82 +220,51 @@ async def illnesses(call: types.CallbackQuery, state: FSMContext):
     data = await state.get_data()
 
     # создаем визуально ожидание
-    wait = await call.message.answer('Обрабатываю вашу информацию⌛')
-    time.sleep(5)
-    wait2 = await call.message.answer("Еще совсем немного")
-    time.sleep(5)
+    wait = await call.message.answer('Обрабатываю вашу информацию...⌛')
+    time.sleep(2)
+    wait2 = await call.message.answer("Еще совсем немного...")
+    time.sleep(4)
     await wait.delete()
     await wait2.delete()
 
     health_status = await check_ur_health(call, state)
     await call.message.answer(f'Ваше имя: {data["name"]}\nВаш возраст: {data["age"]}\n'
                               f'Ваше давление: {data["blood_pressure1"]}/{data["blood_pressure2"]}'
-                              f'\nВаша болезнь: {data["illnesses"]}\n'
-                              f'Заключение:\n{health_status}', reply_markup=kb.main)
+                              '\n'
+                              f'Заключение:\n{health_status}\n'
+                              '\n'
+                              f'Хотели бы вы записаться на прием к врачу?', reply_markup=kb.SMD)
     await state.clear()
 
 
-# @router.callback_query(lambda call: call.data in ["sugar_diabet", "pochki", "serdce", "nothing"])
-# async def handle_illness_selection(query: types.CallbackQuery, state: FSMContext):
-#     user_id = query.from_user.id
-#     selected_illness = query.data
-#     print(user_choices)
-#     print(user_id)
-#     print(selected_illness)
-#
-#     if user_id not in user_choices:
-#         user_choices[user_id] = set()
-#
-#     if selected_illness in user_choices[user_id]:
-#         user_choices[user_id].remove(selected_illness)
-#     else:
-#         user_choices[user_id].add(selected_illness)
-#
-#     # Сохраняем выбранные болезни в состоянии
-#     await state.update_data(illnesses=user_choices[user_id])
-#
-#     updated_keyboard = update_keyboard_text(kb.illnesses_keyboard, user_choices[user_id])
-#
-#     await query.message.edit_text(
-#         'Выберите болезни из списка (можете выбрать несколько):',
-#         reply_markup=updated_keyboard)
-#     await query.answer()
-#
-#     # Обновляем клавиатуру в сообщении
-#     await query.message.edit_reply_markup(reply_markup=updated_keyboard)
+@router.callback_query(lambda call: call.data in ["yes", "no"])
+async def smd(call: types.CallbackQuery, state: FSMContext):
+    doctor_dict = {"yes": "вот список поликлиник в которые вы можете записаться на прием\n"
+                          "- Портал Госуслуги https://gosuslugi.ru/\n"
+                          "- Региональный портал https://talon.zdrav74.ru/\n"
+                          "Контакты регистратур медицинских организаций г. Челябинск:\n"
+                          "ГАУЗ ОТКЗ ГКБ №1 г. Челябинск +7(351)700-24-99\n"
+                          "ГАУЗ ГКБ №2   +7(351)700-00-82\n"
+                          "ГБУЗ ГКБ № 5  +7(351)700-05-00\n"
+                          'ГБУЗ "ГКП №5 г. Челябинск" +7(351)700-75-82\n'
+                          'ГАУЗ "ГКБ №6 г. Челябинск"  +7(351)731-66-66\n'
+                          'ГАУЗ "ГКП №8 г. Челябинск"  +7(351)700-00-33\n'
+                          'ГАУЗ ОЗП ГКБ № 8 +7(351)700-10-80\n'
+                          'ГАУЗ "ГКБ № 9 г. Челябинск" +7(351)700-00-95\n'
+                          'ГАУЗ "ГКБ № 11 г. Челябинск"  +7(351)214-29-29\n'
+                          'ГБУЗ ОКБ №2 +7(351)729-95-10\n'
+                          'ГБУЗ ОКБ №3 +7(351)239-29-18\n'
+                          'ЧУЗ «РЖД-Медицина» г. Челябинск» +7(351)701-62-19n\n'
+                          'ООО «Полимедика» +7(351)240-99-77\n',
+                   "no": ("Регулярное повышение артериального давления отрицательно влияет на ваше здоровье. "
+                          "Рекомендуем обратиться за консультацией к специалисту.")
+                   }
 
+    selected_doc = doctor_dict[call.data]
+    await state.update_data(doctor=selected_doc)
+    data = await state.get_data()
 
-# def update_keyboard_text(keyboard, selected_options):
-#     """Обновляет текст кнопок клавиатуры, добавляя "✅" к выбранным опциям."""
-#     for row in keyboard.inline_keyboard:
-#         for button in row:
-#             if button.callback_data in selected_options:
-#                 if '✅' not in button.text:
-#                     button.text = button.text + ' ✅'
-#             else:
-#                 button.text = button.text.replace(' ✅', '')
-#     return keyboard
-
-
-# @router.callback_query(F.data == "exit")
-# async def exit_illness_selection(call: types.CallbackQuery, state: FSMContext):
-#     user_id = call.from_user.id
-#     data = await state.get_data()
-#
-#     # Используем user_choices[user_id] для получения выбранных болезней
-#     selected_illnesses = ", ".join(user_choices.get(user_id, [])) or "Нет болезней"
-#
-#     health_status = await check_ur_health(call, state)  # Передаем call вместо message
-#     await call.message.answer(
-#         f'Ваше имя: {data["name"]}\n'
-#         f'Ваш возраст: {data["age"]}\n'
-#         f'Ваше давление: {data["blood_pressure1"]}/{data["blood_pressure2"]}\n'
-#         f'Ваши болезни: {selected_illnesses}\n'
-#         f'Заключение:\n{health_status}',
-#         reply_markup=kb.main
-#     )
-#     await state.clear()
-#     await call.message.delete()  # Удаляем сообщение с выбором болезней
+    await call.message.answer(data["doctor"])
 
 
 def categorize_blood_pressure(sys_pressure: int, dia_pressure: int) -> str:
@@ -281,9 +272,9 @@ def categorize_blood_pressure(sys_pressure: int, dia_pressure: int) -> str:
     if sys_pressure >= 180 or dia_pressure >= 110:
         return "АГ 3-й степени"
     elif 160 <= sys_pressure or 100 <= dia_pressure:
-        return "АГ 2-й степени, требуется коррекция лечения, обратитесь к лечащему врачу"
+        return "артериальная гипертензия"
     elif 140 <= sys_pressure or 90 <= dia_pressure:
-        return "АГ 1-й степени, требуется коррекция лечения, обратитесь к лечащему врачу"
+        return "артериальная гипертензия"
     elif sys_pressure >= 140 and dia_pressure < 90:
         return "Изолированная систолическая гипертензия"
     elif sys_pressure < 120 and dia_pressure < 80:
@@ -298,8 +289,8 @@ def categorize_blood_pressure(sys_pressure: int, dia_pressure: int) -> str:
 
 def get_blood_pressure_advice(category: str) -> str:
     advice = {
-        "Оптимальное": "Ваше давление в оптимальном диапазоне. Продолжайте вести здоровый образ жизни.",
-        "Нормальное": "Ваше давление в нормальном диапазоне. Поддерживайте здоровый образ жизни.",
+        "Оптимальное": "Ваш давление в оптимальном диапазоне. Продолжайте вести здоровый образ жизни.",
+        "Нормальное": "Ваш уровень артериального давления в норме.. Поддерживайте здоровый образ жизни.",
         "Высокое нормальное": "Ваше давление немного повышено. "
                               "Рекомендуется обратить внимание на диету и физическую активность.",
         "АГ 1-й степени": "У вас артериальная гипертензия. "
@@ -345,9 +336,9 @@ def check_blood_pressure(age: int, sys_pressure: int, dia_pressure: int):
 async def process_symptoms(message: Message) -> str:
     symptoms = message.text.lower() == "да"
     if symptoms:
-        return "У вас АГ 3-й степени с опасными симптомами. Необходим вызов СМП 112, 103"
+        return "У вас Артериальная гипертензия 3-й степени. Необходим вызов СМП 112, 103"
     else:
-        return "У вас АГ 3-й степени без опасных симптомов. Необходима коррекция терапии, обратитесь к лечащему врачу"
+        return "У вас Артериальная гипертензия 3-й степени. Необходим вызов СМП 112, 103"
 
 
 @router.callback_query(Health.illnesses, lambda call: call.data in ["AG", "sugar_diabet", "pochki", "serdce",
@@ -360,38 +351,15 @@ async def check_ur_health(call: types.CallbackQuery, state: FSMContext):
     dia_pressure = int(data["blood_pressure2"])
     bp_category = categorize_blood_pressure(sys_pressure, dia_pressure)
     illnesses_dict = {
-        "AG": "Артериальная гипертензия",
         "sugar_diabet": "Сахарный диабет",
         "pochki": "Хроническая болезнь почек",
         "serdce": "Ишемическая болезнь сердца",
         "nothing": "Нет болезней"
     }
-    symptoms = data.get("symptoms", False)
 
     selected_ill = illnesses_dict[call.data].lower()
     print(selected_ill)
-
-    # Мгновенная проверка для АГ 3-й степени
-    if bp_category == "АГ 3-й степени":
-        if symptoms:
-            bp_status = "У вас АГ 3-й степени с опасными симптомами. Необходим вызов СМП 112, 103"
-        else:
-            bp_status = ("У вас АГ 3-й степени без опасных симптомов. Необходима коррекция терапии, "
-                         "обратитесь к лечащему врачу")
-        return bp_status
-
-    bp_status = check_blood_pressure(age, sys_pressure, dia_pressure)
-
-    # Обновленное уведомление для болезней высокого риска
-    high_risk_illnesses = ["артериальная гипертензия", "сахарный диабет", "хроническая болезнь почек",
-                           "иемическая болезнь сердца"]
-    if selected_ill in high_risk_illnesses:
-        illness_status = (f"У вас {selected_ill}. Это заболевание связано с высоким риском осложнений, "
-                          f"обратитесь к врачу для срочной консультации.")
-    elif selected_ill == "нет болезней":
-        illness_status = "У вас нет указанных заболеваний."
-    else:
-        illness_status = "Ваше заболевание не указано в списке высокого риска."
+    print(f"вывод: {bp_category}")
 
     age_advice = ""
     if age < 18:
@@ -400,8 +368,27 @@ async def check_ur_health(call: types.CallbackQuery, state: FSMContext):
     elif age > 65:
         age_advice = "В вашем возрасте особенно важно регулярно контролировать давление и консультироваться с врачом."
 
-    return (f"Категория вашего артериального давления: {bp_category}\n"
-            f"{bp_status}\n"
+    print(f"вывод: {bp_category}, тип: {type(bp_category)}")
+    print(f"vivod: {bp_category.split()}")
+
+    if bp_category == "Нормальное" or bp_category == "Оптимальное":
+
+        recomend = ("Ваш уровень артериального давления в норме. "
+                    "Продолжайте придерживаться принципов здорового образа жизни. "
+                    "Если Вам назначены препараты для снижения артериального давления, "
+                    "рекомендуем продолжить их прием.")
+        print(f'рекомендация: {recomend}')
+    else:
+        recomend = ("Ваш уровень артериального давления (АД) выше целевых значений. "
+                    "Рекомендуем Вам регулярно изменять давление утром и вечером, "
+                    "а также вести дневник АД и частоты сердечных сокращений. "
+                    "Требуется назначение или коррекция лечения. "
+                    "Обратитесь за консультацией к лечащему врачу.")
+        print(f'рекомендация: {recomend}')
+
+    return (f"Рекомендуемый уровень артериального давления для Вас:\n"
+            # f"{bp_category}\n"
+            f"{bp_category}\n"
             f"{age_advice}\n"
-            f"{illness_status}\n"
-            f"Рекомендуется регулярно контролировать свое здоровье.")
+            # f"{illness_status}\n"
+            f"{recomend}")
